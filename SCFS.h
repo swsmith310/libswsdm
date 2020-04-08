c2f5f06dfd9225502d43f824c3519d06e561a52a
Spencer Smith <48563701+RevSpennicus@users.noreply.github.com>
2020-04-08 17:59:14 -0300

    Update SCFS.h

--- SCFS.h
+++ SCFS.h
@@ -36,8 +36,7 @@ namespace SCFS
     int t1, t2, t3;
     std::list<SCFS::Flag*> flags;
     void                   init();
-    void                   parse(std::string const& s, const char d, 
-                                 std::vector<std::string>& o1, std::vector<std::string>& o2);
+    void                   parse(std::string const& s, const char d, std::vector<std::string>& o1, std::vector<std::string>& o2);
     static void            addFlag(std::string k, std::string v=0);
     void                   loadFlags();
     void                   updateFlag(std::string k, std::string v=0);
